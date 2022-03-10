147ef18b26803d720660d3527523607b0e8a68fc
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-10 21:20:36 +0100

    Update unify.go

--- go/types/unify.go
+++ go/types/unify.go
@@ -481,7 +481,7 @@ func (u *unifier) nify(x, y Type, p *ifacePair) (result bool) {
 			// we do not require that t.identical(c), but only t.implements(c).
 			// This algorithm therefore mirrors the latter instead of the former.
 			if u.subtyping {
-				return u.implements(x, y, p)
+				return u.implements(x, y, p) == nil
 			}
 
 			xset := x.typeSet()
