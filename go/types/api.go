0187a8d14e0dfe4e9a49bb1248bdde8c2864dfba
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-11 10:05:59 +0100

    Update api.go

--- go/types/api.go
+++ go/types/api.go
@@ -466,7 +466,7 @@ func Implements(V Type, T *Interface) bool {
 	if V.Underlying() == Typ[Invalid] {
 		return false
 	}
-	return (*Checker)(nil).implements(V, T) == nil
+	return new(checkerBase).implements(V, T) == nil
 }
 
 // Identical reports whether x and y are identical types.
